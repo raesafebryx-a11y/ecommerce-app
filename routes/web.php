52081ac3153d739e95d91936ce41b6113c9118ad
raesa f.p <raesafebryx@gmail.com>
2025-12-16 11:44:27 +0700

    migration,model

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,11 @@ Route::get('/produk/{id}', function ($id) {
     return "Detail produk #$id";
 })->name('produk.detail');
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
